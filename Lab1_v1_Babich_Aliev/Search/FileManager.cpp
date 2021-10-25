e095a17f12a76c0728086323d66c4c4bb47aba5d
Aliev Artiom <89365793+Ylidox@users.noreply.github.com>
2021-10-25 09:01:10 +0300

    Add files via upload

---
+++ Lab1_v1_Babich_Aliev/Search/FileManager.cpp
@@ -0,0 +1,64 @@
+#include "FileManager.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+FileManager::FileManager() : path("RUS.txt") {}
+
+FileManager::FileManager(string path)
+{
+	this->path = path;
+}
+
+string FileManager::getPath() {
+	return path;
+}
+
+void FileManager::setPath(string path) {
+	this->path = path;
+}
+
+void FileManager::writeToFile(string record)
+{
+	ofstream out(path, ios::app);
+	if (out.is_open())
+	{
+		out << record << endl;
+	}
+	out.close();
+}
+
+string* FileManager::readFromFile()
+{
+	std::ifstream in(path);
+	string word = "";
+	string* words = new string[length()];
+	if (in.is_open())
+	{
+		int i = 0;
+		while (getline(in, word))
+		{
+			words[i] = word;
+			i++;
+		}
+	}
+	in.close();
+	return words;
+}
+
+int FileManager::length()
+{
+	std::ifstream in(path);
+	string word;
+	int length = 0;
+	if (in.is_open())
+	{
+		while (getline(in, word))
+		{
+			length++;
+		}
+	}
+	in.close();
+	return length;
+}
---
+++ Lab1_v1_Babich_Aliev/Search/Search.cpp
@@ -0,0 +1,53 @@
+﻿#include <regex>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <clocale>
+#include "FileManager.h"
+using namespace std;
+
+int analyz(string str, regex reg) {
+	string lines = str;
+	cmatch result;
+	int size = lines.length();
+	int out = 0;
+	for (int i = 0; ; i++) {
+		regex_search(lines.c_str(), result, reg);
+
+		lines = result.suffix();
+		if (size == lines.length()) break;
+
+		out++;
+		size = lines.length();
+	}
+	return out;
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+
+	FileManager file;
+	file.setPath("file.txt");
+
+	string* lines = file.readFromFile();
+	
+	regex sg("[йфцчвскмпнртгшлщдзжх][яыуаеёиоьъэ]");
+	regex gg("[яыуаеёиоьъэ][яыуаеёиоьъэ]");
+	regex ss("[йфцчвскмпнртгшлщдзжх][йфцчвскмпнртгшлщдзжх]");
+
+	int pattern1 = 0;
+	int pattern2 = 0;
+	int pattern3 = 0;
+
+	int len = file.length();
+	for (int i = 0; i < len; i++) {
+		pattern1 += analyz(lines[i], sg);
+		pattern2 += analyz(lines[i], gg);
+		pattern3 += analyz(lines[i], ss);
+	}
+	cout << pattern1 << endl;
+	cout << pattern2 << endl;
+	cout << pattern3 << endl;
+}
